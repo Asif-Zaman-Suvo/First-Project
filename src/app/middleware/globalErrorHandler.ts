0cf5d0747533e5b9855531c1c0c4be85b331b000
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2023-12-04 11:49:46 +0600

    14-5 Validator things

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,4 +1,9 @@
 import { NextFunction, Request, Response } from 'express'
+import { ZodError } from 'zod'
+import { TErrorSource } from '../interface/error'
+import config from '../config'
+import handleZodError from '../errors/handleZodError'
+import handleValidationError from '../errors/handleValidationError'
 
 const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -8,12 +13,34 @@ const globalErrorHandler = (
   // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
   next: NextFunction,
 ) => {
-  const statusCode = err.statusCode || 500
-  const message = err.message || 'Something went wrong'
+  let statusCode = err.statusCode || 500
+  let message = err.message || 'Something went wrong'
+
+  let errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ]
+  //handle zod error
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorSource = simplifiedError.errorSource
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err)
+    statusCode = simplifiedError?.statusCode
+    message = simplifiedError?.message
+    errorSource = simplifiedError?.errorSource
+  }
+  //ultimate return
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSource,
+    stack: config.NODE_ENV === 'development' ? err?.stack : null,
   })
 }
 
