d7fee70180a8fbd0cfe6d9e32078678d526d1606
Asif-Zaman-Suvo <asifshuvo119181@gmail.com>
2023-12-07 00:46:00 +0600

    module 14 done

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -3,77 +3,29 @@ import { Student } from './student.model'
 import AppError from '../../app/errors/appError'
 import { User } from '../user/user.model'
 import { TStudent } from './student.interface'
+import QueryBuilder from '../../app/builder/QueryBuilder'
+import { studentSearchableFields } from './student.constant'
 
 const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
-  console.log(query, 'base query')
+  const studentQuery = new QueryBuilder(
+    Student.find()
+      .populate('admissionSemester')
+      .populate({
+        path: 'academicDepartment',
+        populate: {
+          path: 'academicFaculty',
+        },
+      }),
+    query,
+  )
+    .search(studentSearchableFields)
+    .filter()
+    .sort()
+    .paginate()
+    .fields()
 
-  const queryObj = { ...query } // copied
-
-  let searchTerm = ''
-  if (query?.searchTerm) {
-    searchTerm = query?.searchTerm as string
-  }
-
-  const studentSearchableFields = ['email', 'name.firstname', 'presentAddress']
-
-  const searchQuery = Student.find({
-    $or: studentSearchableFields.map((field) => ({
-      [field]: { $regex: searchTerm, $options: 'i' },
-    })),
-  })
-
-  //filtering
-  const excludeField = ['searchTerm', 'sort', 'limit', 'page', 'fields']
-
-  excludeField.forEach((el) => delete queryObj[el])
-  console.log({ query, queryObj })
-
-  const filterQuery = searchQuery
-    .find(queryObj)
-    .populate('admissionSemester')
-    .populate({
-      path: 'academicDepartment',
-      populate: {
-        path: 'academicFaculty',
-      },
-    })
-
-  let sort = '-createdAt'
-
-  if (query?.sort) {
-    sort = query?.sort as string
-  }
-
-  const sortQuery = filterQuery.sort(sort)
-
-  let page = 1
-  let limit = 1
-  let skip = 0
-  if (query?.limit) {
-    limit = Number(query.limit)
-  }
-
-  if (query.limit) {
-    limit = Number(query.limit)
-  }
-
-  if (query?.page) {
-    page = Number(query.page)
-    skip = (page - 1) * limit
-  }
-
-  const paginateQuery = sortQuery.skip(skip)
-  const limitQuery = paginateQuery.limit(limit)
-
-  let fields = '-__v'
-
-  if (query.fields) {
-    fields = (query.fields as string).split(',').join(' ')
-  }
-
-  const fieldQuery = await limitQuery.select(fields)
-
-  return fieldQuery
+  const result = await studentQuery.modelQuery
+  return result
 }
 
 const getSingleStudentFromDB = async (id: string) => {
